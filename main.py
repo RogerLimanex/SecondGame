9f5dc9d46bb2c06d087b8cf7b93006c0bad01181
RogerLP <limanex_lp@hotmail.com>
2025-03-29 13:57:29 -0300

    Second commit Class 2

--- main.py
+++ main.py
@@ -5,6 +5,29 @@
 
 import pygame
 
-window = pygame.display.set_mode([800,600])
+window = pygame.display.set_mode([960, 540])
 
 pygame.display.set_caption('Second Game')
+
+background_image = pygame.image.load('D:/Jogos/AtividadePratica/asset/space bg game.png')
+
+player_ship = pygame.image.load('D:/Jogos/AtividadePratica/asset/sprite_nave_pequena.png')
+
+#posição da nave player
+pos_y_player = 400
+pos_x_player = 420
+vel_nave_player = 10
+
+loop = True
+
+while loop:
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            loop = False
+
+    teclas = pygame.key.get_pressed()
+
+    window.blit(background_image, (0, 0))
+    window.blit(player_ship, (pos_y_player, pos_x_player))
+
+    pygame.display.update()
