ef089c0b5b8ed9d27e6be18756a410fe6dc56576
RogerLP <limanex_lp@hotmail.com>
2025-03-29 17:40:15 -0300

    Class 3 Create shot

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import pygame
+import pygame.display
+from pygame import mixer
+from time import sleep
+from random import randint
+from random import random
 
-window = pygame.display.set_mode([960, 540])
-
-pygame.display.set_caption('Second Game')
-
-background_image = pygame.image.load('D:/Jogos/AtividadePratica/asset/space bg game.png')
-
-player_ship = pygame.image.load('D:/Jogos/AtividadePratica/asset/sprite_nave_pequena.png')
-
-enemy_ship = pygame.image.load('D:/Jogos/AtividadePratica/asset/nave_inimiga_pequena.png')
-
-# posição da nave player
+# posição e velocidade da nave player
 pos_y_player = 440
 pos_x_player = 425
-vel_nave_player = 5
+speed_ship_player = 5
+
+# posição e velocidade da nave inimigo
+pos_y_enemy = 440
+pos_x_enemy = 50
+speed_ship_enemy = 5
+
+# posição e velocidade do missil
+pos_y_missil = 450
+pos_x_missil = 430
+speed_x_missil = 10
+
+pontuacao = 0
+
+pygame.init()
+mixer.init()
+
+# Carregamento e inserção dos sons do jogo
+# som_missil = mixer.music.load('nome do laser')
+# som_explosao = pygame.mixer.Sound('nome do som da explosao')
+
+# Carregamento e inserção das imagens do jogo
+background_image = pygame.image.load('D:/Jogos/AtividadePratica/asset/space bg game.png')
+player_ship = pygame.image.load('D:/Jogos/AtividadePratica/asset/sprite_nave_pequena.png')
+enemy_ship = pygame.image.load('D:/Jogos/AtividadePratica/asset/nave_inimiga_pequena.png')
+shot = pygame.image.load('D:/Jogos/AtividadePratica/asset/missil_pequeno.png')
+shot = pygame.transform.scale(shot, (30, 30)) # Transforma o tamanho do objeto
+
+x, y = (960, 540)
+window = pygame.display.set_mode([x, y])
+pygame.display.set_caption('Retorno dos Aliens')
+pygame.display.set_caption('Second Game')
+
+tiro_alvo = False
 
 loop = True
 
@@ -28,16 +56,16 @@ while loop:
 
     # Se pressionar a seta para cima, a nave se moverá para cima
     if keys[pygame.K_UP]:
-        pos_y_player -= vel_nave_player
+        pos_y_player -= speed_ship_player
     # Se pressionar a seta para baixo, a nave se moverá para baixo
     if keys[pygame.K_DOWN]:
-        pos_y_player += vel_nave_player
+        pos_y_player += speed_ship_player
     # Se pressionar a seta para a esquerda, a nave se moverá para a esquerda
     if keys[pygame.K_LEFT]:
-        pos_x_player -= vel_nave_player
+        pos_x_player -= speed_ship_player
     # Se pressionar a seta para a direita, a nave se moverá para a direita
     if keys[pygame.K_RIGHT]:
-        pos_x_player += vel_nave_player
+        pos_x_player += speed_ship_player
 
     if pos_y_player <= -10:
         pos_y_player = -10
@@ -47,7 +75,6 @@ while loop:
         pos_x_player = 0
     if pos_x_player >= 870:
         pos_x_player = 870
-
 
     window.blit(background_image, (0, 0))
     window.blit(player_ship, (pos_x_player, pos_y_player))
